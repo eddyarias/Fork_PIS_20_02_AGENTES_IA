713945e55e48f962f870e1f6f174b7e971b4b198
pablomarcillo <pablo.marcillo@epn.edu.ec>
2023-03-13 17:45:30 -0500

    Add files via upload

---
+++ inference.py
@@ -0,0 +1,27 @@
+import joblib
+import os
+import json
+
+
+def model_fn(model_dir):
+    model = joblib.load(os.path.join(model_dir, "model.joblib"))
+    return model
+
+
+def input_fn(request_body, request_content_type):
+    if request_content_type == 'application/json':
+        request_body = json.loads(request_body)
+        inpVar = request_body['Input']
+        return inpVar
+    else:
+        raise ValueError("This model only supports application/json input")
+
+        
+def predict_fn(input_data, model):
+    return model.predict(input_data)
+
+
+def output_fn(prediction, content_type):
+    res = int(prediction[0])
+    respJSON = {'Output': res}
+    return respJSON
