53e3e4088ae273fada7c53af3dd9be1aaa9712ec
eddya <eddy.arias@epn.edu.ec>
2025-02-28 23:14:00 -0500

    Permitir cors

--- Despliegue_lambda_apigateway_ecr/inference.py
+++ Despliegue_lambda_apigateway_ecr/inference.py
@@ -1,6 +1,6 @@
 from joblib import load
 from boto3 import client
-from json import loads
+from json import loads, dumps
 from os import path
 
 # Configuración de S3
@@ -28,23 +28,51 @@ def lambda_handler(event, context):
         print("Headers:", headers)
         body = event.get("body", {})
         print("Body:", body)
+        
         # Si se usa API Gateway proxy integration, el cuerpo viene en event["body"]
         if "body" in event:
-            # Verificar que el cuerpo no esté vacío
             if event["body"]:
                 body = json.loads(event["body"])
             else:
-                return {'Error': 'El cuerpo de la solicitud (body) está vacío.'}
+                response = {'Error': 'El cuerpo de la solicitud (body) está vacío.'}
+                return {
+                    "statusCode": 400,
+                    "headers": {
+                        "Access-Control-Allow-Origin": "*"
+                    },
+                    "body": json.dumps(response)
+                }
         else:
             body = event
 
         if "Input" not in body:
-            return {'Error': 'No se encontró la clave "Input" en el evento.'}
+            response = {'Error': 'No se encontró la clave "Input" en el evento.'}
+            return {
+                "statusCode": 400,
+                "headers": {
+                    "Access-Control-Allow-Origin": "*"
+                },
+                "body": json.dumps(response)
+            }
         
         input_data = body["Input"]
 
         prediction = model.predict(input_data)
         res = int(prediction[0])
-        return {'Output': res}
+        response = {'Output': res}
+        return {
+            "statusCode": 200,
+            "headers": {
+                "Access-Control-Allow-Origin": "*"
+            },
+            "body": json.dumps(response)
+        }
     except Exception as e:
-        return {'Error en try': str(e)}
+        response = {'Error en try': str(e)}
+        return {
+            "statusCode": 500,
+            "headers": {
+                "Access-Control-Allow-Origin": "*"
+            },
+            "body": json.dumps(response)
+        }
---
+++ ExtraerDatosServicios/lambdaDats/lambda_function.py
@@ -0,0 +1,67 @@
+import json
+import pg8000
+import os
+
+# Variables de entorno para la conexión a la base de datos
+DB_HOST = os.environ['database-2.cdc6eqac283t.eu-north-1.rds.amazonaws.com']
+DB_NAME = os.environ['accidents_uio']
+DB_USER = os.environ['postgres']
+DB_PASSWORD = os.environ['P4ssw0rd123456789']
+
+def lambda_handler(event, context):
+    # Obtener los parámetros de entrada (latitud, longitud, time como entero)
+    latitude = event.get('latitude')
+    longitude = event.get('longitude')
+    time = event.get('time')  # Se espera un entero
+
+    if latitude is None or longitude is None or time is None:
+        return {
+            'statusCode': 400,
+            'body': json.dumps({'message': 'Faltan parámetros. Se requieren latitud, longitud y hora (entero).'})
+        }
+    
+    try:
+        # Conexión a la base de datos usando pg8000
+        conn = pg8000.connect(
+            host=DB_HOST,
+            database=DB_NAME,
+            user=DB_USER,
+            password=DB_PASSWORD
+        )
+        cursor = conn.cursor()
+
+        # Consulta SQL con los parámetros proporcionados
+        query = f"""
+            SELECT
+                get_number_accidents_time({latitude}, {longitude}, {time}) AS num_accidents_time,
+                get_number_accidents_onsitu({latitude}, {longitude}) AS num_accidents_onsitu,
+                get_design_speed_onsitu({latitude}, {longitude}) AS design_speed;
+        """
+        cursor.execute(query)
+        result = cursor.fetchone()
+
+        # Cerrar la conexión
+        cursor.close()
+        conn.close()
+
+        if result:
+            response = {
+                "num_accidents_time": result[0],
+                "num_accidents_onsitu": result[1],
+                "design_speed": result[2]
+            }
+            return {
+                'statusCode': 200,
+                'body': json.dumps(response)
+            }
+        else:
+            return {
+                'statusCode': 404,
+                'body': json.dumps({'message': 'No se encontraron resultados.'})
+            }
+    except Exception as e:
+        print(f"Error: {str(e)}")
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'message': 'Error en la consulta a la base de datos'})
+        }
